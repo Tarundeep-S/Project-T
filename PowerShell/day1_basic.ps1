8c842ce79c4f6c7cfb39b2b8b59d4eb55033c890
Tarundeep Singh <tarun1425t@gmail.com>
2025-06-29 22:04:00 +0530

    Added Day 1 PowerShell script

---
+++ PowerShell/day1_basic.ps1
@@ -0,0 +1,7 @@
+$name = "Tarun"
+$goal = "Become a DevOps + Cloud Engineer"
+Write-Host "Hello, my name is $name and my goal is to $goal!"
+
+For ($i = 1; $i -le 5; $i++) {
+    Write-Host "Day $i of learning PowerShell"
+}

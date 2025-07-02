e19090e41df9e4600a1d0d21f6ac7e21574ba46e
Tarun Deep <tarun1425t@gmail.com>
2025-07-02 06:03:18 +0530

    Modified day4_log_writer.ps1 to trigger workflow

---
+++ PowerShell/day4_log_writer.ps1
@@ -0,0 +1,18 @@
+# Day 4 - PowerShell Log Generator
+
+# Trigger GitHub Actions again
+
+# Triggered via GitHub Actions test
+
+$logFile = "../ProjectT.log"
+
+for ($i = 1; $i -le 5; $i++) {
+    $timestamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+    $message = "[${timestamp}] INFO: Log entry $i added."
+
+    # Append to log file
+    Add-Content -Path $logFile -Value $message
+
+    Write-Host $message
+    Start-Sleep -Seconds 2
+}

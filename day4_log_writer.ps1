ac272ba2e7915639deed1d2c82032adb2e227983
Tarun Deep <tarun1425t@gmail.com>
2025-07-02 05:43:20 +0530

    Fixed log file path in Day 4 script

---
+++ day4_log_writer.ps1
@@ -0,0 +1,14 @@
+# Day 4 - PowerShell Log Generator
+
+$logFile = "../ProjectT.log"
+
+for ($i = 1; $i -le 5; $i++) {
+    $timestamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+    $message = "[${timestamp}] INFO: Log entry $i added."
+
+    # Append to log file
+    Add-Content -Path $logFile -Value $message
+
+    Write-Host $message
+    Start-Sleep -Seconds 2
+}
--- day4_testing_workflow.ps1
+++
@@ -1,4 +0,0 @@
-# Day 4 Script - Testing Workflow
-
-Write-Host "This is Day 4 of Project T"
-Write-Host "Testing Workflow"
